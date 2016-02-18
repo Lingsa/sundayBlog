73ed46fe2c556e5619d3a82d8ac542d2b1539106
sundaySmile <ohtasha@163.com>
2016-02-18 18:52:28 +0800

    hello huifu

--- js/algorithm.js
+++ js/algorithm.js
@@ -1,4 +1,4 @@
-//二分查找算法
+//二分查找(折半查找)算法
 function binarySearch(arr, v, p, r) {
     if((p<r && v>arr[r] || v<arr[p]) || p>r) {
         return undefined;
@@ -45,4 +45,69 @@ function insertSort(arr) {
 }
 
 
-//
+//霍纳规则 (arr为常量数组，x为输入值，y为多项式相加和) y=a0+x(a1+x*(a2+... + x*(a(n-1) + x*an) ) ),arr=[a0,a1,...,an]
+function horner(arr, x) {
+    var i=arr.length-1, y;
+    y=0;
+    while(i>=0) {
+        y=arr[i]+x*y;
+        i--;
+    }
+    return y;
+}
+
+//快速排序
+function quickSort(arr) {
+  if(arr.length<2) return arr;
+  var i,j, key, temVal;
+  i=0;j=arr.length-1;
+  key=arr[0];
+  while(j>i) {
+     if(arr[j]<key) {
+       temVal=arr[i]
+       arr[i]=arr[j]
+       arr[j]=temVal;
+       i++;
+     }
+     j--;
+     
+     if(arr[i]>key && j>i) {
+        temVal=arr[j];
+        arr[i]=arr[j];
+        arr[j]=temVal;
+        j--;
+     }
+     i++;
+  }
+  return arr;
+}
+var testArr=[6,2,7,3,8,9];
+
+function quickSort(array) {
+  function sort(prev, numsize) {
+    var nonius = prev;
+    var j = numsize - 1;
+    var flag = array[prev];
+    if ((numsize - prev) > 1) {
+      while (nonius < j) {
+        for (; nonius < j; j--) {
+          if (array[j] < flag) {
+            array[nonius++] = array[j];　 //a[i] = a[j]; i += 1;
+            break;
+          };
+        }
+        for (; nonius < j; nonius++) {
+          if (array[nonius] > flag) {
+            array[j--] = array[nonius];
+            break;
+          }
+        }
+      }
+      array[nonius] = flag;
+      sort(0, nonius);
+      sort(nonius + 1, numsize);
+    }
+  }
+  sort(0, array.length);
+  return array;
+}
